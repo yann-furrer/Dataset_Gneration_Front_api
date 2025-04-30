a828d7481d8a6277c4edf1287d02cbc9d2899e3a
yann-furrer <72008971+yann-furrer@users.noreply.github.com>
2025-04-30 16:14:14 +0200

    [FIX] Deployment error

--- Front_api/core/checking.py
+++ Front_api/core/checking.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException, Request, Depends
 from fastapi.security import HTTPBearer
 from database.queries.security import check_session_token, check_user_api_token, check_user_suscription_limit, select_dev_token_info
-
+from database.queries.dev_api import select_dev_token_info
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 load_dotenv()
