71fd4a049e4bb47cd7e26df76f50d73f3da85f87
yann-furrer <72008971+yann-furrer@users.noreply.github.com>
2025-06-18 17:11:13 +0200

    [FIX]

--- Front_api/main.py
+++ Front_api/main.py
@@ -10,7 +10,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost:3000",  # Remplacez par votre domaine frontend en développement
     "https://d8bd-2a02-842a-41-2201-b8a0-4372-4c23-4b90.ngrok-free.app",
-    "https://datasetgenerationfront-dev.up.railway.app/dashboard"
+    "https://datasetgenerationfront-dev.up.railway.app"
     # Vous pouvez ajouter d'autres origines si nécessaire
 ]
 
@@ -18,7 +18,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     # allow_credentials=True,
-    access_control_allow_origin=origins,
     # allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     # allow_headers=["Content-Type", "Authorization"],
     allow_credentials=True,
