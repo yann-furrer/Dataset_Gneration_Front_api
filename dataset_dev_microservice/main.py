b4a4386ff46d4db6e646eb6a5a752b7f193478a3
yann-furrer <72008971+yann-furrer@users.noreply.github.com>
2025-10-24 00:10:44 +0200

    [FIX] dataset route api

--- dataset_dev_microservice/main.py
+++ dataset_dev_microservice/main.py
@@ -1,33 +1,41 @@
 from dotenv import load_dotenv
-from routes.dev_token import  dev_token_get , dev_token_post, dev_token_delete
+from routes.dev_token import dev_token_get, dev_token_post, dev_token_delete
 from routes.dataset import dataset_post, dataset_get, dataset_put
-from routes.dataset_config import  dataset_config_get, dataset_config_delete, dataset_config_put
-from routes.rules_config import  rules_config_put, rules_config_delete, rules_config_get
+from routes.dataset_config import (
+    dataset_config_get,
+    dataset_config_delete,
+    dataset_config_put,
+)
+from routes.rules_config import rules_config_put, rules_config_delete, rules_config_get
 from schema.dataset_shema import *
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
+
 load_dotenv()
 app = FastAPI()
 
 # Configuration CORS
 
 origins = [
-    "https://syntetica.net",      # ton site prod
+    "https://syntetica.net",  # ton site prod
     "https://www.syntetica.net",  # version avec www
-    "http://localhost:3000",      # ton front local
+    "http://localhost:3000",  # ton front local
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,               # origines autorisées
-    allow_credentials=True,              # cookies / sessions autorisés
+    allow_origins=origins,  # origines autorisées
+    allow_credentials=True,  # cookies / sessions autorisés
     allow_methods=["GET", "POST", "DELETE", "OPTIONS"],  # méthodes HTTP autorisées
-    allow_headers=["content-type", "authorization", "sessiontoken"],  # headers autorisés
+    allow_headers=[
+        "content-type",
+        "authorization",
+        "sessiontoken",
+    ],  # headers autorisés
 )
-
 
 
 # route api personnalisée
@@ -43,8 +51,11 @@ async def get_dataset_docs():
 async def get_dataset_config_docs():
     """Page Swagger UI spéciale avec seulement certaines routes"""
     return get_swagger_ui_html(
-        openapi_url="/openapi-dataset-config-api.json", title="Documentation REST simplifiée"
+        openapi_url="/openapi-dataset-config-api", title="Documentation REST simplifiée"
     )
+
+
+
 
 @app.get("/rules-api", include_in_schema=False)
 async def get_rules_docs():
@@ -52,7 +63,6 @@ async def get_rules_docs():
     return get_swagger_ui_html(
         openapi_url="/openapi-rules-api.json", title="Documentation REST simplifiée"
     )
-
 
 
 @app.get("/openapi-dataset-api.json", include_in_schema=False)
@@ -65,7 +75,7 @@ async def get_openapi_dataset():
         routes=app.routes,
         description="Cette Api permet d'interrragir avec les services de génération de datasets, la liste des routes est donc utilisable via un token généré sur le site syntetica.net",
     )
-        # Filtre les routes voulues (ici: /special1 et /special2)
+    # Filtre les routes voulues (ici: /special1 et /special2)
     allowed_routes = {"/dev/dataset_list", "/dev/dataset_download_link"}
     full_schema["paths"] = {
         path: schema
@@ -86,7 +96,7 @@ async def get_openapi_rules():
         routes=app.routes,
         description="Cette Api permet d'interrragir avec les services de règles pour la génération de datasets, la liste des routes est donc utilisable via un token généré sur le site syntetica.net",
     )
-        # Filtre les routes voulues (ici: /special1 et /special2)
+    # Filtre les routes voulues (ici: /special1 et /special2)
     allowed_routes = {"/dev/delete_rules_config", "/dev/get_all_rules_data"}
     full_schema["paths"] = {
         path: schema
@@ -97,8 +107,8 @@ async def get_openapi_rules():
     return full_schema
 
 
-@app.get("/openapi-dataset-config-api.json", include_in_schema=False)
-async def get_openapi_dataset_config():
+@app.get("/openapi-dataset-config-api", include_in_schema=False)
+async def get_openapi_test():
     """Schéma OpenAPI restreint"""
     # Crée le schéma complet
     full_schema = get_openapi(
@@ -107,14 +117,23 @@ async def get_openapi_dataset_config():
         routes=app.routes,
         description="Cette Api permet d'interrragir avec les services de dataset configuration pour la génération de datasets, la liste des routes est donc utilisable via un token généré sur le site syntetica.net",
     )
-        # Filtre les routes voulues (ici: /special1 et /special2)
-    allowed_routes = {"/dev/check_dataset_config_is_valid", "/dev/ping_generation_process", "/dev/generate_dataset"}
+    # Filtre les routes voulues (ici: /special1 et /special2)
+    allowed_routes = {
+        "/dev/check_dataset_config_is_valid",
+        "/dev/ping_generation_process",
+        "/dev/generate_dataset",
+    }
     full_schema["paths"] = {
         path: schema
         for path, schema in full_schema["paths"].items()
         if path in allowed_routes
     }
-#Route user pour le dev
+
+    return full_schema
+
+
+
+# Route user pour le dev
 # @app.get("/openapi-rules-config-api.json", include_in_schema=False)
 # async def get_openapi_rules_config():
 #     """Schéma OpenAPI restreint"""
